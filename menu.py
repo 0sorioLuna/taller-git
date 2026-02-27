61bc66588edca9f541433b6a28f1e9ca72963b28
Luna Osorio <lcosorio55@ucatolica.edu.co>
2026-02-27 11:31:43 -0500

    se agraga el ciclo repetitivo al menu

--- menu.py
+++ menu.py
@@ -1,27 +1,26 @@
-num1 = float(input("Ingrese el primer número: "))
-num2 = float(input("Ingrese el segundo número: "))
+while True:
+    print("\n1. Suma")
+    print("2. Resta")
+    print("3. Multiplicación")
+    print("4. División")
+    print("5. Salir")
 
-print("\nSeleccione una operación:")
-print("1. Suma")
-print("2. Resta")
-print("3. Multiplicación")
-print("4. División real")
-print("5. División entera")
-print("6. Módulo")
+    opcion = input("Elija una opción: ")
 
-opcion = int(input("Digite una opción: "))
+    if opcion == "5":
+        print("Saliendo...")
+        break
 
-if opcion == 1:
-    print("Resultado:", num1 + num2)
-elif opcion == 2:
-    print("Resultado:", num1 - num2)
-elif opcion == 3:
-    print("Resultado:", num1 * num2)
-elif opcion == 4:
-    print("Resultado:", num1 / num2)
-elif opcion == 5:
-    print("Resultado:", num1 // num2)
-elif opcion == 6:
-    print("Resultado:", num1 % num2)
-else:
-    print("Opción inválida") 
+    n1 = float(input("Ingrese el primer número: "))
+    n2 = float(input("Ingrese el segundo número: "))
+
+    if opcion == "1":
+        print("Resultado:", n1 + n2)
+    elif opcion == "2":
+        print("Resultado:", n1 - n2)
+    elif opcion == "3":
+        print("Resultado:", n1 * n2)
+    elif opcion == "4":
+        print("Resultado:", n1 / n2)
+    else:
+        print("Opción inválida") 
